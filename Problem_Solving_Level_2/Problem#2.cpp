65de7e90a5014dd629a6ec23abe46143a697710d
Soufiane Ouani <ouanisoufiane@gmail.com>
2024-11-09 15:04:59 +0100

    Update Problem#2.cpp

--- Problem_Solving_Level_2/Problem#2.cpp
+++ Problem_Solving_Level_2/Problem#2.cpp
@@ -3,10 +3,10 @@
 #include <cmath>
 using namespace std;
 
-enum enPrime_NotPrime { Prime = 1, NotPrime = 2 };
+enum enPrime_NotPrime { Prime = 1, NotPrime = 2 }; //We used enums to choose between two options Prime/NotPrime.
 
 int ReadPositiveNumbers(string Message)
-{
+{ //This function is to read only the positive numbers.
 	int Number = 0;
 
 	cout << Message << endl;
@@ -16,7 +16,7 @@ int ReadPositiveNumbers(string Message)
 }
 
 enPrime_NotPrime CheckNumberType(int Number)
-{
+{ //This Function is from the enum type and it's for checking number type and return it.
 	int M = round(Number / 2);
 
 	for (int Counter = 2; Counter <= M; Counter++)
@@ -30,7 +30,9 @@ enPrime_NotPrime CheckNumberType(int Number)
 }
 
 void PrintPrimeNumbersInRange(int Number)
-{
+{    /*This Function is for print all the prime numbers in range
+       for examle if we entered '10' the program will print : (1, 2, 3, 5, 7) */
+	
 	for (int Counter = 1; Counter <= Number; Counter++)
 	{
 
